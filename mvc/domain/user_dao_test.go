d3d2d7e934248eb1881474601468eb871f4f782c
gbubemismith <gsmithwalter@gmail.com>
2022-01-18 00:08:45 +0100

    unit testing in go

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	user, err := GetUser(0)
+
+	assert.Nil(t, user, "we are not expecting a user with id 0")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 was not found", err.Message)
+}
