9a46b19dd6cfe48f60e7651841dcd02bad2b702f
gbubemismith <gsmithwalter@gmail.com>
2022-01-18 00:23:52 +0100

    unit testing in go

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -8,11 +8,27 @@ import (
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
+	//Arrange
+
+	//Act
 	user, err := GetUser(0)
 
+	//Assert
 	assert.Nil(t, user, "we are not expecting a user with id 0")
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
 	assert.EqualValues(t, "not_found", err.Code)
 	assert.EqualValues(t, "user 0 was not found", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Gbubemi", user.FirstName)
+	assert.EqualValues(t, "Smith", user.LastName)
+	assert.EqualValues(t, "ggg@gmail.com", user.Email)
 }
